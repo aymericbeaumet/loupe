c508fd026648f4487ca2db016355c2db41def39e
Aymeric Beaumet <hi@aymericbeaumet.com>
2019-11-05 22:42:22 +0100

    better network view

--- loupe-ui/src/components/Network.js
+++ loupe-ui/src/components/Network.js
@@ -2,11 +2,14 @@ import React, { useEffect, useRef } from "react";
 import Axios from "axios";
 import cytoscape from "cytoscape";
 import dagre from "cytoscape-dagre";
+import popper from "cytoscape-popper";
+import tippy from "tippy.js";
+
+cytoscape.use(dagre);
+cytoscape.use(popper);
 
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
-
-cytoscape.use(dagre);
 
 export default function Network({ query }) {
   const cyRef = useRef(null);
@@ -21,79 +24,11 @@ export default function Network({ query }) {
       params: { query },
       cancelToken: source.token
     })
-      .then(response => {
-        let nextId = 0;
-        const stack = Object.entries(response.data).map(([word, node]) => ({
-          id: nextId++,
-          path: encoder.encode(word),
-          ...node
-        }));
-        const elements = [];
-        let node;
-        while ((node = stack.pop())) {
-          elements.push({
-            classes: "node",
-            data: {
-              id: node.id,
-              content: decoder.decode(Uint8Array.from(node.path))
-            }
-          });
-          for (const [key, child] of node.children) {
-            child.id = nextId++;
-            child.path = [...node.path, key];
-            elements.push({
-              data: { source: node.id, target: child.id }
-            });
-            stack.push(child);
-          }
-          for (const record of node.records) {
-            const recordId = `record:${record.id}`;
-            elements.push({
-              classes: "record",
-              data: {
-                id: recordId,
-                content: record.name
-              }
-            });
-            elements.push({
-              data: {
-                source: node.id,
-                target: recordId
-              }
-            });
-          }
-        }
-        cyRef.current = cytoscape({
+      .then(buildElements)
+      .then(elements => {
+        cyRef.current = renderNetwork({
           container: divRef.current,
-          elements,
-          autoungrabify: true,
-          autounselectify: true,
-          layout: {
-            name: "dagre",
-            nodeDimensionsIncludeLabels: true
-          },
-          style: [
-            {
-              selector: "node",
-              style: {
-                content: "data(content)",
-                "text-valign": "center"
-              }
-            },
-            {
-              selector: "node.record",
-              style: {
-                "background-color": "green"
-              }
-            },
-            {
-              selector: "edge",
-              style: {
-                width: 1,
-                "target-arrow-shape": "triangle"
-              }
-            }
-          ]
+          elements
         });
       })
       .catch(error => {
@@ -104,11 +39,168 @@ export default function Network({ query }) {
 
     return () => {
       source.cancel();
-      if (cyRef.current) {
-        cyRef.current.stop();
-      }
+      cyRef.current && cyRef.current.destroy();
     };
   }, [query]);
 
   return <div ref={divRef} style={{ height: "100vh" }}></div>;
+}
+
+function buildElements(response) {
+  let nextId = 0;
+  const stack = Object.entries(response.data).map(([word, node]) => ({
+    id: nextId++,
+    path: encoder.encode(word),
+    ...node
+  }));
+  const elements = [];
+  let node;
+  while ((node = stack.pop())) {
+    elements.push({
+      classes: "byte",
+      data: {
+        id: node.id,
+        byte: {
+          path: node.path,
+          content: utf8ToUtf16(node.path)
+        }
+      }
+    });
+    for (const [key, child] of node.children) {
+      child.id = nextId++;
+      child.path = [...node.path, key];
+      elements.push({ data: { source: node.id, target: child.id } }); // edge
+      stack.push(child);
+    }
+    for (const record of node.records) {
+      const recordId = `r:${record.id}`;
+      elements.push({
+        classes: "record",
+        data: {
+          id: recordId,
+          record
+        }
+      });
+      elements.push({ data: { source: node.id, target: recordId } }); // edge
+    }
+  }
+  return elements;
+}
+
+function renderNetwork({ container, elements }) {
+  const cy = cytoscape({
+    container,
+    elements,
+    autoungrabify: true,
+    autounselectify: true,
+    motionBlur: true,
+    layout: {
+      name: "dagre",
+      nodeDimensionsIncludeLabels: true
+    },
+    style: [
+      {
+        selector: "node",
+        style: {
+          "text-valign": "center"
+        }
+      },
+      {
+        selector: "node.byte",
+        style: {
+          content: "data(byte.content)"
+        }
+      },
+      {
+        selector: "node.record",
+        style: {
+          content: "data(record.id)",
+          "background-color": "green"
+        }
+      },
+      {
+        selector: "edge",
+        style: {
+          width: 1,
+          "curve-style": "bezier",
+          "target-arrow-shape": "triangle"
+        }
+      }
+    ]
+  });
+
+  cy.ready(() => {
+    cy.elements("node.byte")
+      .unbind("mouseover")
+      .bind("mouseover", ({ target: byte }) => {
+        if (!byte.tippy) {
+          byte.tippy = tippy(byte.popperRef(), {
+            content() {
+              const div = document.createElement("div");
+              div.innerHTML = `[${byte
+                .data("byte")
+                .path.map(n => `0x${n.toString(16).toUpperCase()}`)
+                .join(", ")}]`;
+              return div;
+            },
+            placement: "top",
+            trigger: "manual",
+            hideOnClick: false,
+            multiple: true,
+            sticky: true
+          });
+        }
+        byte.tippy.show();
+      })
+      .unbind("mouseout remove")
+      .bind("mouseout", ({ target: byte }) => byte.tippy.hide());
+
+    cy.elements("node.record")
+      .unbind("mouseover")
+      .bind("mouseover", ({ target: record }) => {
+        if (!record.tippy) {
+          record.tippy = tippy(record.popperRef(), {
+            content() {
+              const div = document.createElement("div");
+              div.innerHTML = JSON.stringify(record.data("record"));
+              return div;
+            },
+            placement: "bottom",
+            trigger: "manual",
+            hideOnClick: false,
+            multiple: true,
+            sticky: true
+          });
+        }
+        record.tippy.show();
+      })
+      .unbind("mouseout remove")
+      .bind("mouseout", ({ target: record }) => record.tippy.hide());
+  });
+
+  cy.on("destroy", () => {
+    cy.elements().forEach(element => {
+      if (element.tippy) {
+        element.tippy.destroy();
+      }
+    });
+  });
+
+  return cy;
+}
+
+// Properly replace � with its hexadecimal counterpart
+function utf8ToUtf16(utf8) {
+  const utf16 = decoder.decode(Uint8Array.from(utf8));
+  let count = 0;
+  while (utf16[utf16.length - count - 1] === "\ufffd") {
+    count++;
+  }
+  if (count > 0) {
+    return `${utf16.slice(0, utf16.length - count)}[${utf8
+      .slice(-count)
+      .map(i => `0x${i.toString(16).toUpperCase()}`)
+      .join(", ")}]`;
+  }
+  return utf16;
 }
