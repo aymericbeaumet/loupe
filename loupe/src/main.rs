033300cf6b6f5e948a504e82de441726de210b0c
Aymeric Beaumet <hi@aymericbeaumet.com>
2019-11-17 23:55:02 +0100

    simplify the main function

--- loupe/src/main.rs
+++ loupe/src/main.rs
@@ -12,14 +12,12 @@ mod services;
 mod tokenizer;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
   pretty_env_logger::init();
 
   let index = index::Index::new();
   let public_service = services::public(index);
   let private_service = services::private(index);
 
-  futures::join!(public_service, private_service); // TODO: main should return this future's result
-
-  Ok(())
+  futures::join!(public_service, private_service);
 }
